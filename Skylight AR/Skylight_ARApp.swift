1e9de47f7dd66fce4e8c51468808387959e63c3c
Hemal Modi <hemal08ce094@gmail.com>
2026-06-04 09:05:19 +0400

    Initial Commit

---
+++ Skylight AR/Skylight_ARApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skylight_ARApp.swift
+//  Skylight AR
+//
+//  Created by Hemal on 04/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Skylight_ARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
